a7ff1a583444cbd9962b6862318b97e087023b41
mariojc <mariojcuello@gmail.com>
2023-06-14 15:42:42 -0400

    ui adjustments

--- src/components/tables/AnswersTable.js
+++ src/components/tables/AnswersTable.js
@@ -1,31 +1,96 @@
 import { useRouter } from "next/router";
 
 const AnswersTable = ({ ipLines, fxsPorts, fxoPorts, callCenterAgents, concurrentCalls, isAcd, isCallCenter, callRecording, pbxModel }) => {
-  
-  const QX50 = {
-    ipLines: 8, fxsPorts: 4, fxoPorts: 4, concurrentCalls: 8, callRecording: 0, isCallCenter: false, callCenterAgents: 0, isAcd: false
-  }
+  const defaultValues = {
+    QX20: {
+      ipLines: { value: 12, unit: "" },
+      maxIpLines: { value: 32, unit: "" },
+      fxsPorts: { value: 0, unit: "" },
+      fxoPorts: { value: 0, unit: "" },
+      concurrentCalls: { value: 10, unit: "" },
+      callRecording: { value: 10, unit: "" },
+      isCallCenter: { value: "-", unit: "" },
+      callCenterAgents: { value: "-", unit: "" },
+      isAcd: { value: "-", unit: "" }
+    },
+    QX50: {
+      ipLines: { value: 16, unit: "" },
+      maxIpLines: { value: 48, unit: "" },
+      fxsPorts: { value: 0, unit: "" },
+      fxoPorts: { value: 0, unit: "" },
+      concurrentCalls: { value: 16, unit: "" },
+      callRecording: { value: 12, unit: "" },
+      isCallCenter: { value: "Yes", unit: "" },
+      callCenterAgents: { value: 30, unit: "" },
+      isAcd: { value: "Yes", unit: "" }
+    },
+    QX60: {
+      ipLines: { value: 24, unit: "" },
+      maxIpLines: { value: 64, unit: "" },
+      fxsPorts: { value: 0, unit: "" },
+      fxoPorts: { value: 0, unit: "" },
+      concurrentCalls: { value: 16, unit: "" },
+      callRecording: { value: 12, unit: "" },
+      isCallCenter: { value: "Yes", unit: "" },
+      callCenterAgents: { value: 30, unit: "" },
+      isAcd: { value: "Yes", unit: "" }
+    },
+    QX100: {
+      ipLines: { value: 32, unit: "" },
+      maxIpLines: { value: 128, unit: "" },
+      fxsPorts: { value: 0, unit: "" },
+      fxoPorts: { value: 0, unit: "" },
+      concurrentCalls: { value: 64, unit: "" },
+      callRecording: { value: 32, unit: "" },
+      isCallCenter: { value: "Yes", unit: "" },
+      callCenterAgents: { value: 30, unit: "" },
+      isAcd: { value: "Yes", unit: "" }
+    },
+    QX200: {
+      ipLines: { value: 24, unit: "" },
+      maxIpLines: { value: 200, unit: "" },
+      fxsPorts: { value: 0, unit: "" },
+      fxoPorts: { value: 0, unit: "" },
+      concurrentCalls: { value: 64, unit: "" },
+      callRecording: { value: 32, unit: "" },
+      isCallCenter: { value: "-", unit: "" },
+      callCenterAgents: { value: 30, unit: "" },
+      isAcd: { value: "Yes", unit: "" }
+    },
+    QX500: {
+      ipLines: { value: 100, unit: "" },
+      maxIpLines: { value: 500, unit: "" },
+      fxsPorts: { value: 0, unit: "" },
+      fxoPorts: { value: 0, unit: "" },
+      concurrentCalls: { value: 80, unit: "" },
+      callRecording: { value: 48, unit: "" },
+      isCallCenter: { value: "-", unit: "" },
+      callCenterAgents: { value: 48, unit: "" },
+      isAcd: { value: "Yes", unit: "" }
+    }
+  };
 
-  
-  const rowData = [ 
-    { label: "Default IP Lines", default: ipLines, userPBX: QX50.ipLines },
-    { label: "FXS Ports", default: fxsPorts, userPBX: QX50.fxsPorts },
-    { label: "FXO Ports", default: fxoPorts, userPBX: QX50.ipLines },
-    { label: "Concurrent Calls", default: concurrentCalls, userPBX: QX50.ipLines },
-    { label: "Call Recording", default: callRecording, userPBX: QX50.ipLines },
-    { label: "Call Center", default: isCallCenter ? "Yes" : "No" , userPBX: QX50.ipLines },
-    { label: "Call Center Agents", default: callCenterAgents },
-    { label: "Automatic Call Distribution", default: isAcd ? "Yes" : "No" , userPBX: QX50.ipLines},
+  const rowData = [
+    { label: "Default IP Lines", userPBX: "-", default: defaultValues[pbxModel].ipLines },
+    { label: "Max IP Lines", userPBX: ipLines, default: defaultValues[pbxModel].maxIpLines },
+    { label: "FXS Ports", userPBX: fxsPorts, default: defaultValues[pbxModel].fxsPorts },
+    { label: "FXO Ports", userPBX: fxoPorts, default: defaultValues[pbxModel].fxoPorts },
+    { label: "Default Concurrent Calls", userPBX: "-", default: defaultValues[pbxModel].concurrentCalls },
+    { label: "Max Concurrent Calls", userPBX: concurrentCalls, default: defaultValues[pbxModel].concurrentCalls },
+    { label: "Max Concurrent Call Recording", userPBX: callRecording, default: defaultValues[pbxModel].callRecording },
+    { label: "Call Center", userPBX: isCallCenter ? "Yes" : "No", default: defaultValues[pbxModel].isCallCenter },
+    { label: "Call Center Agents", userPBX: callCenterAgents, default: defaultValues[pbxModel].callCenterAgents },
+    { label: "Automatic Call Distribution", userPBX: isAcd ? "Yes" : "No", default: defaultValues[pbxModel].isAcd },
   ];
 
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-w-xl ml-auto mr-10">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <div className="w-full overflow-x-auto shadow-md sm:rounded-lg mx-10 my-10">
+      <table className="w-full text-sm  text-gray-500 dark:text-gray-400 ">
         <thead>
-        <tr className="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <th className="px-5 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Feature</th>
-            <th className="px-5 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Included in {pbxModel}</th>
-            <th className="px-5 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Your project</th>
+          <tr className="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <th className="px-5 py-3 font-medium text-left text-gray-900 whitespace-nowrap dark:text-white">Feature</th>
+            <th className="px-5 py-3 text-lg font-medium text-center text-gray-900 whitespace-nowrap dark:text-white">{pbxModel}</th>
+            <th className="px-5 py-3 font-medium text-center text-gray-900 whitespace-nowrap dark:text-white">Your project</th>
           </tr>
         </thead>
         <tbody>
@@ -33,11 +98,13 @@ const AnswersTable = ({ ipLines, fxsPorts, fxoPorts, callCenterAgents, concurren
             <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 font-semibold">
               <th
                 scope="row"
-                className="px-5 py-2 font-md text-gray-900 whitespace-nowrap dark:text-white text-md"
+                className="px-5 py-2 font-md text-left text-gray-900 whitespace-nowrap dark:text-white text-md"
               >
                 {row.label}
               </th>
-              <td className="py-2 text-center text-lg">{row.default}</td>
+              <td className="py-2 text-center text-lg">
+                {row.default.value} {row.default.unit}
+              </td>
               <td className="py-2 text-center text-lg">{row.userPBX}</td>
             </tr>
           ))}
--- src/pages/results.js
+++ src/pages/results.js
@@ -53,9 +53,9 @@ const Results = () => {
   return (
     <>
       <Header></Header>
-      <div className="flex max-w-6xl mx-auto mb-10 items-baseline">
-        <div className="text-5xl font-bold">{pbxModel}</div>
-        <div className="mx-auto">
+      <div className="grid-col-1 md:flex max-w-6xl mx-auto mb-10 items-baseline">
+        <div className=" grid-item text-5xl font-bold ml-10">{pbxModel}</div>
+        <div className=" grid-item mx-auto my-5 md:my-0 justify-center md:items-center">
           <div class="inline-flex rounded-md shadow-sm" role="group">
             <Link
               type="button"
@@ -77,12 +77,12 @@ const Results = () => {
         <Link
           href={"/"}
           onClick={""}
-          className="px-4 py-2 text-md font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+          className="px-4 py-2 mr-10 text-md font-medium hidden md:flex text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
         >
           Start Over
         </Link>
       </div>
-      <div className="flex mx-auto items-center max-w-6xl">
+      <div className="flex mx-auto items-center max-w-6xl gap-20">
         
         <AnswersTable
           ipLines={ipLines}
@@ -95,7 +95,7 @@ const Results = () => {
           isAcd={isAcd}
           pbxModel={pbxModel}
         ></AnswersTable>
-        <img src={`/img/${pbxModel}.png`} alt={pbxModel} className="mx-auto" />
+        <img src={`/img/${pbxModel}.png`} alt={pbxModel} className=" mr-20 lg:flex hidden" />
       </div>
     </>
   );
