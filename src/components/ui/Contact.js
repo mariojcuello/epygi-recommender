00352c79fe89f7473c336dd2e07e2be96c52c078
mariojc <mariojcuello@gmail.com>
2023-08-01 11:51:57 -0400

    test send and contact

--- src/components/ui/Contact.js
+++ src/components/ui/Contact.js
@@ -2,8 +2,10 @@ import { useState, handleSubmit } from "react";
 
 const Contact = () => {
   {
-    //const [fullname, setFullname] = useState("");
+    const [fullname, setFullname] = useState("");
     const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
 
     //   Form validation state
     const [errors, setErrors] = useState({});
@@ -19,9 +21,20 @@ const Contact = () => {
       let tempErrors = {};
       let isValid = true;
 
-      
+      if (fullname.length <= 0) {
+        tempErrors["fullname"] = true;
+        isValid = false;
+      }
       if (email.length <= 0) {
         tempErrors["email"] = true;
+        isValid = false;
+      }
+      if (subject.length <= 0) {
+        tempErrors["subject"] = true;
+        isValid = false;
+      }
+      if (message.length <= 0) {
+        tempErrors["message"] = true;
         isValid = false;
       }
 
@@ -40,6 +53,9 @@ const Contact = () => {
         const res = await fetch("/api/sendgrid", {
           body: JSON.stringify({
             email: email,
+            fullname: fullname,
+            subject: subject,
+            message: message,
           }),
           headers: {
             "Content-Type": "application/json",
@@ -59,23 +75,44 @@ const Contact = () => {
         setShowFailureMessage(false);
         setButtonText("Send");
       }
-      console.log(email);
+      console.log(fullname, email, subject, message);
     };
 
     return (
-      <div className="mx-auto max-w-screen-lg grid grid-cols-1 md:grid-cols-2 my-20">
-        <div>
-          {/* <h2 class="mb-4 text-2xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
-            Contact Us
-          </h2> */}
-          <p className=" font-light text-center text-gray-500 dark:text-gray-300 text-xl mx-auto mb-10">
-            Please enter your email to send us a request for the configuration you have created.
-          </p>
-        </div>
-
-        <form onSubmit={handleSubmit} className="mx-auto flex">
-          <div className="w-[300px]">
-            
+      <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+          Contact Me
+        </h2>
+        <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          Need help with a project? Hiring? Just want to chat? Drop a line here!
+        </p>
+        <form onSubmit={handleSubmit} class="space-y-8">
+          <div>
+            <label
+              for="email"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Your Name
+            </label>
+            <input
+              type="fullname"
+              id="fullname"
+              value={fullname}
+              onChange={(e) => {
+                setFullname(e.target.value);
+              }}
+              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="What's your name?!"
+              required
+            />
+          </div>
+          <div>
+            <label
+              for="email"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Your email
+            </label>
             <input
               type="email"
               id="email"
@@ -83,30 +120,61 @@ const Contact = () => {
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="your@email.com"
               required
             />
           </div>
-
-          <div className="flex justify-center max-h-12 pl-2">
-            <button
-              type="submit"
-              className="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          <div>
+            <label
+              for="subject"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
-              Submit
-            </button>
-            {showSuccessMessage && (
-              <div className="text-xl ml-5 animate-bounce">
-                Thank you for your message!
-              </div>
-            )}
-            {showFailureMessage && (
-              <div className="text-xl ml-5 text-red-500 animate-bounce">
-                Message Failed!
-              </div>
-            )}
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              value={subject}
+              onChange={(e) => {
+                setSubject(e.target.value);
+              }}
+              class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="What's up!"
+              required
+            />
           </div>
+          <div class="sm:col-span-2">
+            <label
+              for="message"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              Your message
+            </label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              rows="6"
+              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              placeholder="No really, whats up!"
+            ></textarea>
+          </div>
+          <div className="flex items-baseline">
+           <button
+            type="submit"
+            className="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+            {showSuccessMessage && <div className="text-xl ml-5 animate-bounce">Thank you for your message!</div>}
+            {showFailureMessage && <div className="text-xl ml-5 text-red-500 animate-bounce">Message Failed!</div>} 
+          </div>
+          
+          
+          
         </form>
       </div>
     );
